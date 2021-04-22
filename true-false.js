4b826b4c437e739d08923c2fbf26d8699c4228e9
arif322 <arifshumon.ewu@gmail.com>
2021-04-22 22:59:53 +0600

    truthy and falsy value added

---
+++ true-false.js
@@ -0,0 +1,13 @@
+// const age = 0; 
+//let num = null;
+
+//let num = NaN;
+
+const num = 12;
+
+if(num || num==0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}

dcb5add03a7b9075cd943909d746b40e8ec7037a
arif322 <arifshumon.ewu@gmail.com>
2021-04-22 23:45:39 +0600

    null and undefined value

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+let pakhi;
+function add(num1,num2){
+    console.log(num1+num2);
+}
+
+function add(num1,num2){
+    console.log(num1, num2);
+}
+const result = add(10);
+console.log(result);
+
+let fun = undefined;
+console.log(fun);
+
+let ages =[1,2,3]
+console.log(ages[11]);
